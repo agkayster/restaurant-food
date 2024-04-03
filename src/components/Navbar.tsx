8cd00e305294b13210d0ca82216fb56f6d5298be
agkayster <ejike.chiboka@gmail.com>
2024-04-03 20:33:28 +0100

    Implementation of Navbar for medium, large and extra large screens

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,42 @@
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 
 import Menu from './Menu';
+import CartIcon from './CartIcon';
 
 const Navbar = () => {
+	// Temporary
+	const user = false;
+
 	return (
-		<div className='h-12 text-red-500 p-4 flex flex-row items-center justify-between border-b-2 border-b-red-500 uppercase'>
+		<div className='h-12 text-red-500 p-4 flex flex-row items-center justify-between border-b-2 border-b-red-500 uppercase md:h-24 lg:px-10 xl:px-20'>
+			{/* LEFT LINKS */}
+			<div className='hidden md:flex gap-4 flex-1'>
+				<Link href='/'>Home</Link>
+				<Link href='/menu'>Menu</Link>
+				<Link href='/contact'>Contact</Link>
+			</div>
 			{/* LOGO */}
-			<div className='text-lg font-semibold'>
+			<div className='text-lg font-semibold flex-1 md:text-center'>
 				<Link href='/'>blueflame</Link>
 			</div>
 			{/* MOBILE MENU */}
-			<div>
+			<div className='md:hidden'>
 				<Menu />
+			</div>
+			{/* RIGHT LINKS */}
+			<div className='hidden md:flex gap-4 items-center justify-end flex-1 md:relative'>
+				<div className='md:absolute md:top-[-4.5rem] md:right-2 lg:static flex items-center gap-2 cursor-pointer bg-orange-300 px-1 rounded-md'>
+					<Image src='/phone.png' alt='' width={20} height={20} />
+					<span>+234-8022229876</span>
+				</div>
+				{!user ? (
+					<Link href='/login'>Login</Link>
+				) : (
+					<Link href='/orders'>Orders</Link>
+				)}
+				<CartIcon />
 			</div>
 		</div>
 	);
