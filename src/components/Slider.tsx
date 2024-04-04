2b30d52ba69ae9d678f068b06218636c519cef3b
agkayster <ejike.chiboka@gmail.com>
2024-04-04 08:13:15 +0100

    implemented dynamic version of slider in home page

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,8 +1,12 @@
 'use client';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 
 const SliderComponent = () => {
+	/* initial start for our slides */
+	const [currentSlide, setCurrentSlide] = useState(0);
+
+	/* Data for different slides */
 	const data = [
 		{
 			id: 1,
@@ -17,25 +21,38 @@ const SliderComponent = () => {
 		{
 			id: 3,
 			title: 'the best pizza to share with your family',
-			image: '/slide3.jpg',
+			image: '/slide3.jpeg',
 		},
 	];
 
+	/* changes our images and slides every 2 secs */
+	useEffect(() => {
+		const interval = setInterval(
+			() =>
+				setCurrentSlide((prev) =>
+					prev === data.length - 1 ? 0 : prev + 1
+				),
+			2000
+		);
+
+		return () => clearInterval(interval);
+	}, [data.length]);
+
 	return (
-		<div className='flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)]'>
+		<div className='flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] lg:flex-row bg-fuchsia-50'>
 			{/* TEXT */}
-			<div className='h-1/2 flex items-center justify-center flex-col gap-8 font-bold text-red-500'>
-				<h1 className='text-5xl text-center uppercase p-4 md:text-6xl lg:text-7xl'>
-					Test
+			<div className='flex-1 flex items-center justify-center flex-col gap-8 md:gap-6 font-bold text-red-500'>
+				<h1 className='text-4xl text-center uppercase p-4 md:p-10 lg:text-5xl'>
+					{data[currentSlide].title}
 				</h1>
-				<button className='bg-red-500 text-white py-4 px-8'>
+				<button className='bg-red-500 text-white py-4 px-8 md:mb-12'>
 					Order now
 				</button>
 			</div>
 			{/* IMAGE */}
-			<div className='h-1/2 w-full relative'>
+			<div className='flex-1 relative'>
 				<Image
-					src='/slide1.png'
+					src={data[currentSlide].image}
 					alt='alt'
 					fill
 					className='object-cover'
