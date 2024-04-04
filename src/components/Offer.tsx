415224d1057989f2b6aebd3ba0bdb7364de3d870
agkayster <ejike.chiboka@gmail.com>
2024-04-04 22:24:01 +0100

    Static implementation of Offers section

--- src/components/Offer.tsx
+++ src/components/Offer.tsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import Image from 'next/image';
 
 function OfferComponent() {
-	return <div>Offer</div>;
+	return (
+		<div className='flex flex-col md:flex-row bg-black h-screen'>
+			{/* TEXT */}
+			<div className='flex-1 flex flex-col items-center justify-center gap-4 text-center p-6'>
+				<h1 className='text-white text-5xl font-bold'>
+					Delicious Burger & French Fry
+				</h1>
+				<p className='text-white lg:text-xl'>
+					Progressively simplify effective e-toilers and
+					process-centric methods of empowerment. Quickly pontificate
+					parallel.
+				</p>
+				<button className='bg-red-500 text-white rounded-md py-3 px-6'>
+					Order Now
+				</button>
+			</div>
+			{/* IMAGE */}
+			<div className='relative flex-1 w-full md:h-full'>
+				<Image
+					src='/offerProduct.png'
+					alt='alt'
+					fill
+					className='object-contain'
+				/>
+			</div>
+		</div>
+	);
 }
 
 export default OfferComponent;
