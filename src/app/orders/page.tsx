5d1b82e91f73ed1715d255c00d184ba3fc7dddfe
agkayster <ejike.chiboka@gmail.com>
2024-04-15 15:36:50 +0100

    Implement static version of Order status input element

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -4,9 +4,13 @@ import { useQuery } from '@tanstack/react-query';
 import { OrderType } from '@/types/types';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
+import Image from 'next/image';
 
 const OrdersPage = () => {
 	const { data: session, status } = useSession();
+
+	/* shows details of the logged in user */
+	console.log('get session in orders =>', session);
 
 	const router = useRouter();
 
@@ -21,6 +25,16 @@ const OrdersPage = () => {
 		queryFn: () =>
 			fetch('http://localhost:3000/api/orders').then((res) => res.json()),
 	});
+
+	const handleSubmit = (
+		e: React.FormEvent<HTMLFormElement>,
+		orderId: string
+	) => {
+		e.preventDefault();
+		const form = e.target as HTMLFormElement;
+		const input = form.elements[0] as HTMLInputElement;
+		const status = input.value;
+	};
 
 	if (isPending || status === 'loading') return 'Loading...';
 
@@ -57,7 +71,7 @@ const OrdersPage = () => {
 										{id}
 									</td>
 									<td className='py-6 px-1'>
-										{createdAt.toString()}
+										{createdAt.toString().slice(0, 10)}
 									</td>
 									<td className='py-6 px-1'>{price}</td>
 									{products.map(({ title }) => (
@@ -67,7 +81,33 @@ const OrdersPage = () => {
 											{title}
 										</td>
 									))}
-									<td className='py-6 px-1'>{status}</td>
+									{session?.user.isAdmin ? (
+										<td>
+											<form
+												className='flex flex-row gap-2 items-center justify-center'
+												onSubmit={(e) =>
+													handleSubmit(e, id)
+												}>
+												<input
+													placeholder={status}
+													type='text'
+													className='p-2 ring-1 ring-red-100 rounded-md'
+												/>
+												<button
+													type='submit'
+													className='bg-red-400 p-2 rounded-full'>
+													<Image
+														src='/edit.png'
+														alt='alt'
+														width={20}
+														height={20}
+													/>
+												</button>
+											</form>
+										</td>
+									) : (
+										<td className='py-6 px-1'>{status}</td>
+									)}
 								</tr>
 							)
 						)}
