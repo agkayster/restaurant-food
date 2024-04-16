0336c5759b2e7ebdce0e3a6e6fd8981699028375
agkayster <ejike.chiboka@gmail.com>
2024-04-16 22:32:50 +0100

    Implemented dynamic Cart functionality in Navbar

--- src/components/CartIcon.tsx
+++ src/components/CartIcon.tsx
@@ -1,14 +1,17 @@
+'use client';
+import { useCartStore } from '@/utils/store';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
 const CartIcon = () => {
+	const { totalItems } = useCartStore();
 	return (
 		<div className='flex gap-4 items-center'>
 			<div className='relative w-8 h-8 md:w-5 md:h-5'>
 				<Image src='/cart.png' alt='cart icon' fill sizes='100vw' />
 			</div>
-			<span>Cart (3)</span>
+			<span>Cart ({totalItems})</span>
 		</div>
 	);
 };
