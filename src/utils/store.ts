a87f7f7cb80f1e142cb4d6a0755d6bf423e138e1
agkayster <ejike.chiboka@gmail.com>
2024-04-17 07:03:07 +0100

    implementation of persistency in the dynamic cart functionality

--- src/utils/store.ts
+++ src/utils/store.ts
@@ -1,5 +1,6 @@
 import { ActionTypes, CartType } from '@/types/types';
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
 /* we must declare our initial state */
 const INITIAL_STATE = {
@@ -8,28 +9,33 @@ const INITIAL_STATE = {
 	totalPrice: 0,
 };
 
-export const useCartStore = create<CartType & ActionTypes>((set, get) => ({
-	products: INITIAL_STATE.products,
-	totalItems: INITIAL_STATE.totalItems,
-	totalPrice: INITIAL_STATE.totalPrice,
+export const useCartStore = create(
+	persist<CartType & ActionTypes>(
+		(set, get) => ({
+			products: INITIAL_STATE.products,
+			totalItems: INITIAL_STATE.totalItems,
+			totalPrice: INITIAL_STATE.totalPrice,
 
-	/* we take the item and push into our products array, everything goes into the cart */
-	addToCart(item) {
-		set((state) => ({
-			products: [...state.products, item],
-			totalItems: state.totalItems + item.quantity, // => add up total items in our cart
-			totalPrice: state.totalPrice + item.price, // => add up total price in our cart
-		}));
-	},
+			/* we take the item and push into our products array, everything goes into the cart */
+			addToCart(item) {
+				set((state) => ({
+					products: [...state.products, item],
+					totalItems: state.totalItems + item.quantity, // => add up total items in our cart
+					totalPrice: state.totalPrice + item.price, // => add up total price in our cart
+				}));
+			},
 
-	/* remove item(s) from cart using filter method */
-	removeFromCart(item) {
-		set((state) => ({
-			products: state.products.filter(
-				(product) => product.id !== item.id
-			),
-			totalItems: state.totalItems - item.quantity, // => once item is removed, reduce total items
-			totalPrice: state.totalPrice - item.price, // => once item is removed, reduce total price
-		}));
-	},
-}));
+			/* remove item(s) from cart using filter method */
+			removeFromCart(item) {
+				set((state) => ({
+					products: state.products.filter(
+						(product) => product.id !== item.id
+					),
+					totalItems: state.totalItems - item.quantity, // => once item is removed, reduce total items
+					totalPrice: state.totalPrice - item.price, // => once item is removed, reduce total price
+				}));
+			},
+		}),
+		{ name: 'cart' }
+	)
+);
