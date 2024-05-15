27599acb6fa9c9ae2d4a875be70242536f188dcc
agkayster <ejike.chiboka@gmail.com>
2024-05-15 16:29:01 +0100

    implemented more webpack code

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,69 @@
+// import { webpack } from 'next/dist/compiled/webpack/webpack';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
 		domains: ['res.cloudinary.com'],
 	},
+	webpack: (config, { isServer }) => {
+		if (!isServer) {
+			config.resolve.fallback = {
+				...config.resolve.fallback,
+				stream: require.resolve('stream-browserify'),
+				crypto: require.resolve('crypto-browserify'),
+			};
+
+			config.plugins.push(
+				new webpack.ProvidePlugin({
+					process: 'process/browser',
+				}),
+				new webpack.NormalModuleReplacementPlugin(
+					/node:crypto/,
+					(resource) => {
+						resource.request = resource.request.replace(
+							/^node:/,
+							''
+						);
+					}
+				)
+			);
+		}
+		return config;
+	},
 };
 
 export default nextConfig;
+
+// const webpack = require('webpack');
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+// 	reactStrictMode: true,
+
+// 	webpack: (config, { isServer }) => {
+// 		if (!isServer) {
+// 			config.resolve.fallback = {
+// 				...config.resolve.fallback,
+// 				stream: require.resolve('stream-browserify'),
+// 				crypto: require.resolve('crypto-browserify'),
+// 			};
+
+// 			config.plugins.push(
+// 				new webpack.ProvidePlugin({
+// 					process: 'process/browser',
+// 				}),
+// 				new webpack.NormalModuleReplacementPlugin(
+// 					/node:crypto/,
+// 					(resource) => {
+// 						resource.request = resource.request.replace(
+// 							/^node:/,
+// 							''
+// 						);
+// 					}
+// 				)
+// 			);
+// 		}
+// 		return config;
+// 	},
+// };
+
+// module.exports = nextConfig;
